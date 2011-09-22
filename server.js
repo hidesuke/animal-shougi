4d384c7c1032ffdb2f5f34d04b5be612651217a1
hidesuke <hidesuke@gmail.com>
2011-09-22 23:23:46 +0900

    websocket周りのコードを少しだけ書いた

--- server.js
+++ server.js
@@ -3,10 +3,12 @@
  * Module dependencies.
  */
 
-var express = require('express');
+var express = require('express'),
+    json    = JSON.stringify;
+var app     = module.exports = express.createServer();
+var io      = require('socket.io').listen(app);
 
-var app = module.exports = express.createServer();
-
+// -------------------------------------------------------
 // Configuration
 
 app.configure(function(){
@@ -26,13 +28,31 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
+// -------------------------------------------------------
 // Routes
 
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Express'
+    title: 'どうぶつしょうぎのもり'
   });
 });
 
-app.listen(3000);
+if (app.setting.env === 'production') {
+  app.listen(80);
+} else {
+  app.listen(3000);
+}
+
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+
+// -------------------------------------------------------
+// WebSocket
+
+io.sockets.on('connection', function(socket) {
+  socket.on('login', function() {
+  });
+});
+
+
+
