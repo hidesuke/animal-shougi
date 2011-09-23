bdb536806954e4f5c09243fbd7877d11d03edbcc
hidesuke <hidesuke@gmail.com>
2011-09-23 13:26:03 +0900

    盤面初期化ロジック追加

---
+++ public/javascripts/animal-shougi.js
@@ -0,0 +1,12 @@
+function initBoard() {
+  $('#a1').html('<div class="giraffe">馬</div>');
+  $('#b1').html('<div class="lion">獅</div>');
+  $('#c1').html('<div class="elephant">象</div>');
+  $('#b2').html('<div class="chick">雛</div>');
+  $('#b3').html('<div class="chick">雛</div>');
+  $('#a4').html('<div class="elephant">象</div>');
+  $('#b4').html('<div class="lion">獅</div>');
+  $('#c4').html('<div class="giraffe">馬</div>');
+}
+
+
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,10 +5,52 @@
 // ----------------------------------------------
 // Websocket Process
 var socket = io.connect('http://' + location.host);
+var columnIndexArray = ['a', 'b', 'c'];
 
 socket.on('connect', function() {
-  socket.on('board', function(){
-    alert('Debug : socket.on board'); 
+  socket.on('board', function(data){
+    // 現在の盤面
+    var i, j;
+    var currentBoard = data.board;
+    for (i = 0; i < 3; i++) {
+      for (j = 0; j < 4; j++){
+        if(currentBoard[i][j]) {
+          var piece = currentBoard[i][j];
+          var temp = j + 1;
+          var boardIndex = "#" + columnIndexArray[i] + temp;
+          var pieceKind = piece.charAt(1);
+          var pieceClass, pieceName;
+          switch(pieceKind){
+          case 'L' :
+            pieceClass = "lion";
+            pieceName = "獅";
+            break;
+          case 'E' :
+            pieceClass = "elephant";
+            pieceName = "象";
+            break;
+          case 'G' :
+            pieceClass = "giraffe"
+            pieceName = "馬";
+            break;
+          case 'C' :
+            pieceClass = "chick";
+            pieceName = "雛";
+            break;
+          case 'P' :
+            pieceClass = "pollo";
+            pieceName = "鶏";
+            break;
+          default :
+            break;
+          }
+          $(boardIndex).html('<div class="' + pieceClass + '">' + pieceName + '</div>');
+        }
+      }
+    }
+    
+    // どっちの手番かを取得して表示してね
+    var currentTurn = data.turn;
   });
 
   socket.on('result', function(){
@@ -31,6 +73,7 @@ var loginName = "";
 
 function init(){
   setEvent();
+  initBoard();
 }
 
 // setEvent
